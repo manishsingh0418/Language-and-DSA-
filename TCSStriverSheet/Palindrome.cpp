dae5746244cccf3c9ac91d05eab85d3690f3bb74
manishsingh0418 <manishkumar80819@gmail.com>
2024-09-18 21:35:58 +0530

    Palindrome

---
+++ TCSStriverSheet/Palindrome.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    // Code here.
+		    int temp =n;
+		    int reverse =0;
+		    while(n>0)
+		    {
+		        reverse = (reverse*10+(n%10));
+		        n=n/10;
+		    }
+		    if(reverse==temp)
+		    {
+		        return "Yes";
+		    }
+		    return "No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
