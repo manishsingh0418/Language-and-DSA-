59c25b33cc8d0bc9fda3230e4fa7debe814442ca
manishsingh0418 <manishkumar80819@gmail.com>
2024-09-29 20:37:45 +0530

    Factorial

---
+++ TCSStriverSheet/Factorial.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    long long int factorial(int N){
+        //code here
+        long long fact=1;
+        for(int i=N;i>0;i--)
+        {
+            fact=fact*i;
+        }
+        return fact;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
