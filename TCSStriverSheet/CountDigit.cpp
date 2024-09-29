8735ebb9a123b3b5478d2b6b5769bc86b26c09dc
manishsingh0418 <manishkumar80819@gmail.com>
2024-09-29 20:55:47 +0530

    Count Digit

---
+++ TCSStriverSheet/CountDigit.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int count =0;
+        int original=N;
+        while(N>0)
+        {
+            int a=N%10;
+            if(a!=0 && original%a==0)
+            {
+            count++;
+            }
+            N=N/10;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
