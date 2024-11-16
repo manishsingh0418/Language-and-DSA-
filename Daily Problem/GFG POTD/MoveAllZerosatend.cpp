858183d291cf2631d2875f7f418f3ecf96503e1b
manishsingh0418 <manishkumar80819@gmail.com>
2024-11-16 23:54:02 +0530

    potd

---
+++ Daily Problem/GFG POTD/MoveAllZerosatend.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int i=0,j;
+       for(j=0;j<n;j++){
+           if(arr[j]!=0){
+               swap(arr[i],arr[j]);
+               i++;
+           }
+       }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int n = arr.size();
+        ob.pushZerosToEnd(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
