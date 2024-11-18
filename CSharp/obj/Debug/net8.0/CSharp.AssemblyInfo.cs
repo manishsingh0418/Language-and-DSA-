7b55d5cb3f0dc53bc6727d5c419b7bc9f35569e7
manishsingh0418 <manishkumar80819@gmail.com>
2024-11-18 23:46:35 +0530

    potd

--- CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
+++ CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5462ab12828c37cac9328169d5ff4773776fdbfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503a2881d8733bf754d2f56bbdea0bfdb875311d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
