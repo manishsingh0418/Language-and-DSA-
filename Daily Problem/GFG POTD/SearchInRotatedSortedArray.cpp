c8d8b142634289d32f65c609a5e9ca0d8b963a12
manishsingh0418 <manishkumar80819@gmail.com>
2024-12-14 23:47:21 +0530

    potd

---
+++ Daily Problem/GFG POTD/SearchInRotatedSortedArray.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    int solve(vector<int>& arr, int low, int high, int k)
+    {
+        if(low > high) // if the array has been searched and element not found check
+        {
+            return -1;
+        }
+        int mid = low+((high-low)>>1); // calculate the mid using bitwise for little more efficiency
+        if(arr[mid] == k) return mid; // return index when element is found
+        if(arr[low] <= arr[mid]) // left half is sorted search here
+        {
+            if(arr[low] <= k && k <= arr[mid]) // check if the key is present in this range
+            {
+                return solve(arr, low, mid-1, k); // shrink the right pointer to mid-1
+            }
+            else // key is present in the right side of the array
+            {
+                return solve(arr, mid+1, high, k); // shrink the left pointer to mid+1
+            }
+        }
+        else // right half is sorted search here
+        {
+            if(k >= arr[mid] && k <= arr[high]) // check if key lies in right array range
+            {
+                return solve(arr, mid+1, high, k); //shrink the left pointer to mid+1
+            }
+            else // key is present in the left half
+            {
+                return solve(arr, low, mid-1, k); // shrink the right pointer to mid-1
+            }
+        }
+    }
+    int search(vector<int>& arr, int key) {
+        // complete the function here
+        return solve(arr, 0, arr.size()-1, key); // send array, low, high and the key
+        
+    }
+};
